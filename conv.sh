ce4700f0ae5af5daffe18a69f1fbea4212eb349e
Alexios Kantartzis <alexikan98@gmail.com>
2022-11-23 19:01:45 +0200

    Update conv.sh

--- conv.sh
+++ conv.sh
@@ -7,7 +7,7 @@ genFormat(){
 		echo "	Done!"
 	elif [ ${format} = "pdf" ]; then
 		echo "	Generating pdf file..."
-		pandoc -N --variable "geometry=margin=1.1in" --variable mainfont="OpenSans-Regular.ttf" --variable fontsize=13pt --variable version=2.0 ${file}  --pdf-engine=xelatex --toc -o ${file::len-3}.pdf;
+		pandoc -N --variable "geometry=margin=1.1in" --variable mainfont="OpenSans-Regular.ttf" --variable fontsize=13pt --variable version=2.0 ${file}  --pdf-engine=xelatex --toc -o ${file::len-3}.pdf
 		echo "	Done!"
 	else
 		echo "	Error: Wrong Argument."
